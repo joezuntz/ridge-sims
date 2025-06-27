950a72c9ec3bac137ea80de7aec5a17040331e89
Joe Zuntz <joezuntz@googlemail.com>
2025-06-27 12:47:18 +0100

    add some docstrings etc

--- example_run.py
+++ example_run.py
@@ -7,6 +7,12 @@ import matplotlib.pyplot as plt
 from mpi4py.MPI import COMM_WORLD
 
 def load_coordinates(base_sim_dir, run_id, shift=True):
+    """
+    Load coordinates from a catalog file.
+
+    If shift is True, the right ascension (RA) is shifted by 180 degrees.
+    Use shift=True when finding filaments, but not for building density maps.
+    """
     filename = os.path.join(base_sim_dir, f"run_{run_id}", "lens_catalog_0.npy")
     with h5py.File(filename, 'r') as f:
         ra = f["RA"][:]
@@ -24,6 +30,9 @@ base_sim_dir = "lhc_run_sims"
 
 
 def results_plot(density_map, ridges, plot_filename):
+    """
+    Make a plot of a density map and ridge points on top.
+    """
     healpy.cartview(density_map, min=0, lonra=[20, 50], latra=[-40, -10],)
     healpy.graticule()
 
@@ -35,11 +44,14 @@ def results_plot(density_map, ridges, plot_filename):
 
 
 def build_density_map(base_sim_dir, run_id, nside, smoothing_degrees=0.5):
+    """
+    Make a density maps from the coordinates.
+    """
     # The healpy conventions are different and should not have
     # the 180 deg shift applied
     data = load_coordinates(base_sim_dir, run_id, shift=False)
-    dec = data[:, 0]
-    ra = data[:, 1]
+    dec = np.degrees(data[:, 0])
+    ra = np.degrees(data[:, 1])
     npix = healpy.nside2npix(nside)
     pix = healpy.ang2pix(nside, ra, dec, lonlat=True)
     m = np.zeros(npix, dtype=int)
@@ -47,9 +59,9 @@ def build_density_map(base_sim_dir, run_id, nside, smoothing_degrees=0.5):
     m1 = healpy.smoothing(m, fwhm=np.radians(smoothing_degrees), verbose=False)
     return m1
 
-def redo_cuts(ridges, initial_density, final_density):
-    cut1 = initial_density > initial_density.max() * 0.4
-    cut2 = final_density > final_density.max() * 0.05
+def redo_cuts(ridges, initial_density, final_density, initial_percentile=0, final_percentile=25):
+    cut1 = initial_density > np.percentile(initial_density, initial_percentile)
+    cut2 = final_density > np.percentile(final_density, final_percentile)
     return ridges[cut1 & cut2]
 
 
@@ -58,9 +70,11 @@ def main():
     bandwidth = 0.5
     neighbours = 5000
     convergence = 1e-5
+    seed = 3482364
     mesh_size = None
     run_id = 1
-    filename = f"joe5/ridges_run_{neighbours}_{bandwidth}.npz"
+    checkpoint_dir = "example/checkpoints"
+    filename = f"example/ridges_run_{neighbours}_{bandwidth}.npz"
 
     coordinates = load_coordinates(base_sim_dir, run_id)
         
@@ -70,9 +84,9 @@ def main():
                             distance_metric='haversine',
                             n_neighbors=neighbours,
                             comm=COMM_WORLD,
-                            checkpoint_dir=None,
+                            checkpoint_dir=checkpoint_dir,
                             resume=True,
-                            seed=3482364,
+                            seed=seed,
                             mesh_size=mesh_size
                             )
     
