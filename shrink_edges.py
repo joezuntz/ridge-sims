a24681296d53643ad1832393426347538959c74c
Mehraveh <mehraveh.n@gmail.com>
2025-09-03 07:43:49 +0200

    parameter change

--- shrink_edges.py
+++ shrink_edges.py
@@ -19,8 +19,8 @@ os.makedirs(output_dir, exist_ok=True)
 # --- parameters ---
 nside = 512
 arcmin = np.pi / 180.0 / 60.0
-radius_arcmin = 4.0          # radius of the disk filter
-min_coverage = 0.8           # minimum coverage fraction allowed (0–1)
+radius_arcmin = 8.0          # radius of the disk filter
+min_coverage = 1           # minimum coverage fraction allowed (0–1)
 
 
 def load_mask(mask_filename, nside):
