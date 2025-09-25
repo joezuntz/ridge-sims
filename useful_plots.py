c9e091eae2cf1ac78b1ba9f31a4f454e5aaa10be
Mehraveh <mehraveh.n@gmail.com>
2025-09-24 21:42:30 -0400

    BG & filaments plots

--- useful_plots.py
+++ useful_plots.py
@@ -199,7 +199,7 @@ filament_h5 = os.path.join(filament_dir, "filaments_p15.h5")
 
 with h5py.File(filament_h5, "r") as hdf:
     dataset = hdf["data"]
-    ra_values = (dataset["RA"][:] + 180) % 360
+    ra_values = dataset["RA"][:] 
     dec_values = dataset["DEC"][:]
     labels = dataset["Filament_Label"][:]
 
