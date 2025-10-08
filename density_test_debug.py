c4e76ff01107c3d98fac8c07afcd7129e338769e
Mehraveh <mehraveh.n@gmail.com>
2025-10-08 07:18:55 +0200

    post density cut treatment temporary code

--- density_test_debug.py
+++ density_test_debug.py
@@ -21,6 +21,30 @@ def results_plot(density_map, ridges, plot_filename):
     plt.savefig(plot_filename, bbox_inches='tight', dpi=300)
     plt.close()
     print(f"Saved plot → {plot_filename}")
+    
+
+def build_density_map(base_sim_dir, run_id, nside, smoothing_degrees=0.5):
+    """
+    Make a density map from the coordinates.
+    """
+    filename = os.path.join(base_sim_dir, f"run_{run_id}", "lens_catalog_0.npy")
+    with h5py.File(filename, 'r') as f:
+        ra = f["RA"][:]
+        dec = f["DEC"][:]
+        z_true = f["Z_TRUE"][:]
+    mask = z_true < 0.4
+    ra = ra[mask]
+    dec = dec[mask]
+    data = np.column_stack((dec, ra))
+    dec = np.degrees(data[:, 0])
+    ra = np.degrees(data[:, 1])
+    npix = healpy.nside2npix(nside)
+    pix = healpy.ang2pix(nside, ra, dec, lonlat=True)
+    m = np.zeros(npix, dtype=int)
+    np.add.at(m, pix, 1)
+    m_smooth = healpy.smoothing(m, fwhm=np.radians(smoothing_degrees), verbose=False)
+    return m_smooth
+
 
 
 def redo_cuts_safe(ridges, initial_density, final_density,
