90bd430bfa50a7da9d892cf7899128d045b1c8d4
Mehraveh <mehraveh.n@gmail.com>
2025-12-02 20:42:54 +0100

    lsst ridges

---
+++ lsst_ridge_finder_slurm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=lsst_ridges
+#SBATCH --output=logs/lsst_ridges_%A_%a.out
+#SBATCH --error=logs/lsst_ridges_%A_%a.err
+
+#SBATCH --array=1-8
+#SBATCH --nodes=1               
+#SBATCH --ntasks=16             # 16 MPI ranks
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=512G
+#SBATCH --time=10-00:00:00
+
+source ~/.bashrc
+conda activate /home/jzuntz/ridges/env
+
+mpirun -n $SLURM_NTASKS python lsst_ridges_slurm.py --task-id $SLURM_ARRAY_TASK_ID
