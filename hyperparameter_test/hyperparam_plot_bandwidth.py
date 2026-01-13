fcc53d522300c57a2f1cbe6a74f551c8c8c94bdb
Mehraveh <mehraveh.n@gmail.com>
2026-01-13 21:57:09 +0100

    hyperparameter plots

--- hyperparameter_test/hyperparam_plot_bandwidth.py
+++ hyperparameter_test/hyperparam_plot_bandwidth.py
@@ -2,6 +2,8 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import matplotlib.cm as cm
 
 # ------------------------------------------------------------
 # Publication-style parameters
